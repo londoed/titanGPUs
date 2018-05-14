16150fb48c1aee9499ec304aa1fd63410c144f0a
londoed <londoed@comcast.net>
2018-05-14 01:27:19 -0400

    read/write added

--- src/titan_GPUs.py
+++ src/titan_GPUs.py
@@ -20,6 +20,7 @@
 
 #       Include modules
 
+import numpy as np
 
 #       Configuration variables
 
@@ -30,6 +31,8 @@ cabs = []
 #       Define file locations
 
 pict_file = "dc16.html"
+data_file = "dc12.txt"
+service_file = "services.txt"
 
 
 #       Initialize cabinets
@@ -52,34 +55,34 @@ while x < x_dim:
 
 #       Update picture
 
-def update_pict():
+def update_pict(cabs):
     x,y,c,s,n = [0,0,0,0,0]
     cabs = []
     num_2_replace = 0
-    file1 = open("dc16.txt", 'r')
-    # print("in Update\n")
-    node = file1.readlines()
-    for nd in node:
-        num_2_replace += 1
-        if nd[2] == '-':
-            x = nd[1],
-            y = nd[3],
-            c = nd[5],
-            s = nd[7],
-            n = nd[9],
-        else:
-            x = 1 * nd[1] + nd[2],
-            y = nd[4],
-            c = nd[6],
-            s = nd[8],
-            n = nd[10],
+    with open(data_file, 'r') as file:
 
-#       Print cabs
-    return cabs[x][y][c][s][n] = "<td bgcolor=\"green\">n</td>"
-    file1.close()
+        # print("in Update\n")
+        node = file.readlines()
+        for nd in node:
+            num_2_replace += 1
+            if nd[2] == '-':
+                x = nd[1],
+                y = nd[3],
+                c = nd[5],
+                s = nd[7],
+                n = nd[9],
+            else:
+                x = 1 * nd[1] + nd[2],
+                y = nd[4],
+                c = nd[6],
+                s = nd[8],
+                n = nd[10],
 
-    file2 = open("service.txt", 'r')
-        node = file2.readlines()
+    #       Print cabs
+        return cabs[x][y][c][s][n] = "<td bgcolor=\"green\">n</td>"
+
+    with open(service_file, 'r') as file:
+        node = file.readlines()
         bgc = 'purple'
         for nd in node:
             num_2_replace += 1
@@ -98,15 +101,14 @@ def update_pict():
 
     #       Update picture
 
-    cabs[x][y][c][s][n] = "<td bgcolor=\"bgc\">n</td>";
-    file2.close()
+        return cabs[x][y][c][s][n] = "<td bgcolor=\"bgc\">n</td>";
+
 
 #       Print picture
 
-def print_pict():
+def print_pict(cabs):
     x,y,c,s,n = [0,0,2,0,0]
     cabs = []
-    open(pict_file, 'w')
     html_str = """
     <HTML>
       <HEAD>
@@ -117,34 +119,32 @@ def print_pict():
       <font size=+8><pre>GPU Replacement status</pre>Details</font></a>
       <TABLE BORDER=10 WIDTH=\"100%\" NOSAVE bordercolordark=\"blue\" bordercolorlight=\"gold\">"
     """
-
-    html_file = open(pict_file, 'w')
-    html_file.write(html_str)
-    while y < 8:
-        y += 1
-        html_file.append("<tr>")
-        while x < x_dim:
-            x += 1
-            html_file.append("<th>cx-y</th>")
-                while x < x_dim:
-                    x += 1
-                    html_file.append("<td><table cols=8 width=\"100%\">\n")
-                    while c >= 0:
-                        c -= 1
-                        while n < 4:
-                            n += 1
-                            html_file.append("<tr>")
-                            while s < 8:
-                                s += 1
-                                html_file.append(cabs[x][y][c][s][n])
-                                if n == 3 and c != 0:
-                                    html_file.append("<tr><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td></tr>")
-                                else:
-                                    html_file.append("</tr>\n")
-                    html_file.append("</table></td>\n")
-        html_file.append("</tr>")
-    html_file.append("</table></body></html>")
-    html_file.close()
+    with open(pict_file, 'w') as html_file:
+        html_file.write(html_str)
+        while y < 8:
+            y += 1
+            html_file.append("<tr>")
+            while x < x_dim:
+                x += 1
+                html_file.append("<th>cx-y</th>")
+                    while x < x_dim:
+                        x += 1
+                        html_file.append("<td><table cols=8 width=\"100%\">\n")
+                        while c >= 0:
+                            c -= 1
+                            while n < 4:
+                                n += 1
+                                html_file.append("<tr>")
+                                while s < 8:
+                                    s += 1
+                                    html_file.append(cabs[x][y][c][s][n])
+                                    if n == 3 and c != 0:
+                                        html_file.append("<tr><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td></tr>")
+                                    else:
+                                        html_file.append("</tr>\n")
+                            html_file.append("</table></td>\n")
+                html_file.append("</tr>")
+        html_file.append("</table></body></html>")
 
 update_pict()
 print_pict()
